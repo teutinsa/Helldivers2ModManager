eef54c295efc03d4047323bbae5fde3f747331b2
Florian Schumacher <FloCo12@gmx.net>
2024-12-20 20:24:35 +0100

    - Fixed the sub-option combo box changing options on scroll
    Finished new help page

---
+++ Helldivers2ModManager/ComboBoxScrollBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Helldivers2ModManager;
+
+public static class ComboBoxScrollBehavior
+{
+	public static readonly DependencyProperty DisableScrollProperty = DependencyProperty.RegisterAttached("DisableScroll", typeof(bool), typeof(ComboBoxScrollBehavior), new PropertyMetadata(false, OnDisableScrollChanged));
+
+	public static bool GetDisableScroll(DependencyObject obj)
+	{
+		return (bool)obj.GetValue(DisableScrollProperty);
+	}
+
+	public static void SetDisableScroll(DependencyObject obj, bool value)
+	{
+		obj.SetValue(DisableScrollProperty, value);
+	}
+
+	private static void OnDisableScrollChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+	{
+		if (sender is ComboBox comboBox)
+		{
+			if ((bool)e.NewValue)
+				comboBox.PreviewMouseWheel += ComboBox_PreviewMouseWheel;
+			else
+				comboBox.PreviewMouseWheel -= ComboBox_PreviewMouseWheel;
+		}
+	}
+
+	private static void ComboBox_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+	{
+		e.Handled = true;
+	}
+}
