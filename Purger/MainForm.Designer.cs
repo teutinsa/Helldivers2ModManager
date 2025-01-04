60fac0c64b314d59e98ac3bf4551cc1761594dd3
Florian Schumacher <FloCo12@gmx.net>
2025-01-04 05:46:49 +0100

    Added Purger tool as a band aid for crashes

---
+++ Purger/MainForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Purger;
+
+partial class MainForm
+{
+	/// <summary>
+	/// Required designer variable.
+	/// </summary>
+	private System.ComponentModel.IContainer components = null;
+
+	/// <summary>
+	/// Clean up any resources being used.
+	/// </summary>
+	/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing && (components != null))
+		{
+			components.Dispose();
+		}
+		base.Dispose(disposing);
+	}
+
+	#region Windows Form Designer generated code
+
+	/// <summary>
+	/// Required method for Designer support - do not modify
+	/// the contents of this method with the code editor.
+	/// </summary>
+	private void InitializeComponent()
+	{
+		txtGameDir = new TextBox();
+		label1 = new Label();
+		btnBrowse = new Button();
+		progressBar = new ProgressBar();
+		btnPurge = new Button();
+		folderDialog = new FolderBrowserDialog();
+		SuspendLayout();
+		// 
+		// txtGameDir
+		// 
+		txtGameDir.Location = new Point(12, 27);
+		txtGameDir.Name = "txtGameDir";
+		txtGameDir.ReadOnly = true;
+		txtGameDir.Size = new Size(222, 23);
+		txtGameDir.TabIndex = 0;
+		// 
+		// label1
+		// 
+		label1.AutoSize = true;
+		label1.Location = new Point(12, 9);
+		label1.Name = "label1";
+		label1.Size = new Size(89, 15);
+		label1.TabIndex = 1;
+		label1.Text = "Game Directory";
+		// 
+		// btnBrowse
+		// 
+		btnBrowse.Location = new Point(240, 27);
+		btnBrowse.Name = "btnBrowse";
+		btnBrowse.Size = new Size(32, 23);
+		btnBrowse.TabIndex = 2;
+		btnBrowse.Text = "...";
+		btnBrowse.UseVisualStyleBackColor = true;
+		btnBrowse.Click += btnBrowse_Click;
+		// 
+		// progressBar
+		// 
+		progressBar.Location = new Point(12, 56);
+		progressBar.Name = "progressBar";
+		progressBar.Size = new Size(260, 23);
+		progressBar.Step = 5;
+		progressBar.Style = ProgressBarStyle.Continuous;
+		progressBar.TabIndex = 3;
+		// 
+		// btnPurge
+		// 
+		btnPurge.Enabled = false;
+		btnPurge.Location = new Point(12, 85);
+		btnPurge.Name = "btnPurge";
+		btnPurge.Size = new Size(260, 23);
+		btnPurge.TabIndex = 4;
+		btnPurge.Text = "Purge";
+		btnPurge.UseVisualStyleBackColor = true;
+		btnPurge.Click += btnPurge_Click;
+		// 
+		// folderDialog
+		// 
+		folderDialog.Description = "Select your Helldivers 2 installation location";
+		// 
+		// MainForm
+		// 
+		AutoScaleDimensions = new SizeF(7F, 15F);
+		AutoScaleMode = AutoScaleMode.Font;
+		ClientSize = new Size(284, 120);
+		Controls.Add(btnPurge);
+		Controls.Add(progressBar);
+		Controls.Add(btnBrowse);
+		Controls.Add(label1);
+		Controls.Add(txtGameDir);
+		FormBorderStyle = FormBorderStyle.FixedSingle;
+		MaximizeBox = false;
+		Name = "MainForm";
+		Text = "Purger";
+		ResumeLayout(false);
+		PerformLayout();
+	}
+
+	#endregion
+
+	private TextBox txtGameDir;
+	private Label label1;
+	private Button btnBrowse;
+	private ProgressBar progressBar;
+	private Button btnPurge;
+	private FolderBrowserDialog folderDialog;
+}
---
+++ Purger/MainForm.cs
@@ -0,0 +1,94 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Purger;
+
+public partial class MainForm : Form
+{
+	public MainForm()
+	{
+		InitializeComponent();
+	}
+
+	private static bool ValdiatePath(string path, [NotNullWhen(false)] out string? err)
+	{
+		var dir = new DirectoryInfo(path);
+		var dirs = dir.GetDirectories();
+
+		var binDir = dirs.FirstOrDefault(static d => d.Name == "bin");
+		if (binDir is null)
+		{
+			err = "The selected folder does not have a directory named \"bin\"!";
+			return false;
+		}
+
+		if (!binDir.GetFiles().Any(static f => f.Name == "helldivers2.exe"))
+		{
+			err = "The selected folders \"bin\" folder does not contain a file called \"helldivers2.exe\"!";
+			return false;
+		}
+
+		if (!dirs.Any(static d => d.Name == "data"))
+		{
+			err = "The selected folder does not have a directory named \"bin\"!";
+			return false;
+		}
+
+		if (!dirs.Any(static d => d.Name == "tools"))
+		{
+			err = "The selected folder does not have a directory named \"tools\"!";
+			return false;
+		}
+
+		err = null;
+		return true;
+	}
+
+	private void SafeInvoke(Action action)
+	{
+		if (InvokeRequired)
+			Invoke(action);
+	}
+
+	async void btnPurge_Click(object sender, EventArgs e)
+	{
+		btnBrowse.Enabled = false;
+		btnPurge.Enabled = false;
+		progressBar.Value = 0;
+
+		var count = await Task.Run(() =>
+		{
+			var dir = new DirectoryInfo(Path.Combine(txtGameDir.Text, "data"));
+			var files = dir.EnumerateFiles("*.patch_*").ToArray();
+
+			SafeInvoke(() => progressBar.Maximum = files.Length);
+
+			foreach (var f in files)
+			{
+				f.Delete();
+				SafeInvoke(() => progressBar.Value++);
+			}
+
+			return files.Length;
+		});
+
+		MessageBox.Show(this, $"Deleted {count} files!\nIt's recommended to verify your game files now.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+		btnBrowse.Enabled = true;
+		btnPurge.Enabled = true;
+	}
+
+	void btnBrowse_Click(object sender, EventArgs e)
+	{
+		if (folderDialog.ShowDialog() != DialogResult.OK)
+			return;
+
+		if (!ValdiatePath(folderDialog.SelectedPath, out var err))
+		{
+			MessageBox.Show(this, err, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			return;
+		}
+
+		txtGameDir.Text = folderDialog.SelectedPath;
+		btnPurge.Enabled = true;
+	}
+}
